bae5e99c47988b734fe25b0cded2b48971d40c86
Yuliia-Mykhailovska <lisa-yulyalist@gmail.com>
2024-02-13 19:54:30 +0200

    Update Завд2,мод3.py
    зі змінами після перевірки

--- Завд2,мод3.py
+++ Завд2,мод3.py
@@ -1,9 +1,9 @@
 import random
 def get_numbers_ticket(min, max, quantity):
     try:
-        if min>=1 and max<=1000:
+        if min >= 1 and max <= 1000:
             lottery_numbers = set()
-            while len(lottery_numbers)<quantity:
+            while len(lottery_numbers) < quantity:
                 lottery_numbers.add(random.randint(min, max))
         return sorted(list(lottery_numbers))
     except:
